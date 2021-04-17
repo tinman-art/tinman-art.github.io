4f365716084a8100e700c888948d9c28c53c1c93
Tiago Borica <tiago.borica@gmail.com>
2021-04-17 04:36:09 -0300

    Várias alterações

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 
-var imagesAnimateTime = 0150;
-var fadeInOutTime = 300;
+$( document ).ready(function(){
+    var elem = document.querySelector('.grid');
+    var elem = document.querySelector('.grid2');
+    var iso = new Isotope( elem, {
+      // options
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows'
+    });
+    
+    var iso2 = new Isotope( '.grid2', {
+        // options
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+    });
+
+
+});
+
+let modal = new bootstrap.Modal(document.getElementById("modal-7"));
+
+function openModal() {
+  modal.show();
+}
+
+let imagesAnimateTime = 0150;
+let fadeInOutTime = 300;
 
 $(".category1").hover(function() {
         $(".category1").animate({"width":"105%"}, imagesAnimateTime);
@@ -25,11 +49,17 @@ $(".category2").mouseout(function() {
 });
 
 $(".category1").click(function() {
-    $(".menu").fadeOut(fadeInOutTime);
+    $(".menu").hide();
     $(".gallery1").fadeIn(fadeInOutTime);
 })
 
 $(".category2").click(function() {
-    $(".menu").fadeOut(fadeInOutTime);
+    $(".menu").hide();
     $(".gallery2").fadeIn(fadeInOutTime);
 })
+
+function showMenu() {
+    $(".gallery1").hide();
+    $(".gallery2").hide();
+    $(".menu").fadeIn(fadeInOutTime);
+}
